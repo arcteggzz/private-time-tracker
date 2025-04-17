13cf4b47c6080955dd1ae291452d930223c158f1
Oghenetega Esedere <arcteggzz@gmail.com>
2025-04-17 22:22:49 +0100

    Feat: added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Route, Routes, useLocation } from "react-router-dom";
+import NavMenu from "./components/NavMenu";
 import ButtonWithModal from "./components/ButtonWithModal";
 import DistanceWalkedTracker from "./components/DistanceWalkedTracker";
 
@@ -8,8 +9,9 @@ function App() {
   return (
     <>
       <Routes location={location} key={location.pathname}>
-        <Route path={"/"} element={<ButtonWithModal />} />
-        <Route path={"/v2"} element={<DistanceWalkedTracker />} />
+        <Route path={"/"} element={<NavMenu />} />
+        <Route path={"v2/app_1"} element={<ButtonWithModal />} />
+        <Route path={"v2/app_2"} element={<DistanceWalkedTracker />} />
       </Routes>
     </>
   );
---
+++ src/components/NavMenu.tsx
@@ -0,0 +1,32 @@
+import { Link } from "react-router-dom";
+
+function NavMenu() {
+  const links = [
+    {
+      title: "Simpleton",
+      route: "/v2/app_1",
+    },
+    {
+      title: "Walks",
+      route: "/v2/app_2",
+    },
+  ];
+
+  return (
+    <>
+      <div className="w-screen h-screen flex flex-col items-center justify-center font-[Inter] gap-2">
+        {links.map((link) => (
+          <Link
+            key={link.route}
+            to={link.route}
+            className="bg-[#6e56b6] text-white px-4 py-2 rounded-lg mt-6 cursor-pointer"
+          >
+            {link.title}
+          </Link>
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default NavMenu;
